0f8c87839f496f7a067955a32a09a442642f8b75
Borbiro <43454691+Borbiro@users.noreply.github.com>
2024-03-04 22:13:07 +0100

    Post view completed

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ import data from './data/blog_posts.json' assert {type: 'json'};
 const app = express()
 const port = 3000
 
-//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
     res.render("index.ejs", {data: data})
+})
+
+app.post('/post', (req, res) => {
+  var postNumber = req.body["post-number"]
+  res.render("post.ejs", {data: data[postNumber]})
+})
+
+app.get('/create', (req, res) => {
+  res.render("create_post.ejs")
 })
 
 app.listen(port, () => {
