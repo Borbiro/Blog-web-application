9c25c9fb808e30160e8247b20af202285ba3f073
Borbiro <43454691+Borbiro@users.noreply.github.com>
2024-03-05 21:55:17 +0100

    Completed requirements
    Update check
    Delete post check

--- functions.js
+++ functions.js
@@ -1,9 +1,16 @@
-export function createBlogPost(newData, jsonFile) {
+export function createBlogPost(newData) {
     var appendData = newData
-    appendData["id"] = jsonFile.length + 1
     return appendData
 }
 
-export function changeBlogPost(newData, jsonFile) {
-    
+export function changeBlogPost(newData, postNumber, jsonFile) {
+    var updatedData = jsonFile
+    updatedData[postNumber] = newData
+    return updatedData
+}
+
+export function deleteBlogPost(postNumber, jsonFile) {
+    var updatedData = jsonFile
+    updatedData.splice(postNumber, 1)
+    return updatedData
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import data from './data/blog_posts.json' assert {type: 'json'};
-import { createBlogPost } from "./functions.js";
+import { createBlogPost, changeBlogPost, deleteBlogPost } from "./functions.js";
 import fs from "fs";
 
 const app = express()
@@ -17,7 +17,10 @@ app.get('/', (req, res) => {
 
 app.post('/post', (req, res) => {
   var postNumber = req.body["post-number"]
-  res.render("post.ejs", {data: data[postNumber]})
+  res.render("post.ejs", {
+    data: data[postNumber],
+    postNumber: postNumber
+  })
 })
 
 app.get('/create', (req, res) => {
@@ -26,8 +29,31 @@ app.get('/create', (req, res) => {
 })
 
 app.post('/submit', (req, res) => {
-  data.push(createBlogPost(req.body, data))
+  data.push(createBlogPost(req.body))
   fs.writeFileSync('./data/blog_posts.json', JSON.stringify(data), 'utf-8')
+  res.render("completed.ejs")
+})
+
+app.post('/change', (req, res) => {
+  var postNumber = req.body["post-number"]
+  res.render("change.ejs", {
+    data: data[postNumber],
+    postNumber: postNumber})
+})
+
+app.post("/changePost", (req, res) => {
+  var changedData = {
+    body: req.body.body,
+    title: req.body.title,
+    subtitle: req.body.subtitle,
+    image_url: req.body.imgLink
+  }
+  fs.writeFileSync('./data/blog_posts.json', JSON.stringify(changeBlogPost(changedData, req.body["post-number"], data)), 'utf-8')
+  res.render("completed.ejs")
+})
+
+app.post('/delete', (req, res) => {
+  fs.writeFileSync('./data/blog_posts.json', JSON.stringify(deleteBlogPost(req.body["post-number"], data)), 'utf-8')
   res.render("completed.ejs")
 })
 
